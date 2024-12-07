156371ac5829539d937ff822a0b5e16b8c8d371c
Shahzad Waris <160225124+meshahzad92@users.noreply.github.com>
2024-12-07 12:43:35 +0500

    chnged constatnts values

--- Files/constants.py
+++ Files/constants.py
@@ -1,4 +1,4 @@
-my_email = 'mshahzadwaris92@gmail.com'
-password = 'Shahzad@92'
+my_email = 'Your_email'
+password = 'Your_password'
 login_page = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
 Profile_url='https://www.linkedin.com/in/hammad-naveed-a310782/'
